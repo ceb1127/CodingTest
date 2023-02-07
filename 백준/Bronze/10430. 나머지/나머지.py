64ec4207d8f8151e91c3a8b34e2aeb453bf02b5c
eunbi <62796977+ceb1127@users.noreply.github.com>
2023-02-07 18:57:18 +0900

    [Bronze V] Title: 나머지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,4 +1,6 @@
 A,B,C = map(int,input().split())
 
-print((A+B)%C, ((A%C)+(B%C))%C, (A*B)%C, ((A%C)*(B%C))%C, sep='\n')
-# sep='\n'로 줄바꿈
+print( (A+B)%C )
+print( ((A%C)+(B%C))%C)
+print( (A*B)%C )
+print( ((A%C)*(B%C))%C )
