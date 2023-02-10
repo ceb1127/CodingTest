6d48fbfc0f2d3489c66b7ca4014ced8c67e10ca9
eunbi <62796977+ceb1127@users.noreply.github.com>
2023-02-10 17:42:09 +0900

    [Silver V] Title: 소수 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,19 +1,20 @@
-n = int(input()) #테스트 케이스 입력
-
-sosu = list(map(int,input().split()))
-
-def prime(num) : #소수 함수 생성
-    if num == 1:
-        return False # 1은 소수가 아님 
-    elif num == 2:
-        return True # 2는 소수
-    for i in range(2,num) : 
-        if num % i == 0 : #2부터 num까지 숫자로 나누어 떨어지는 수는 소수가 아님
+def prime(x) :
+    if x < 2 :
+        return False
+    i = 2
+    while i*i <= x:
+        if x % i == 0:
             return False
+        i += 1
     return True
-count = 0
 
-for i in sosu :
-    if prime(i) : #프라임 함수에 입력받은 값을 넣었을 때
-        count += 1  #true 값이라면 count 증가
-print(count)
+
+n = int(input())
+a = list(map(int, input().split()))
+answer = 0
+
+for x in  a:
+    if prime(x):
+        answer += 1
+print(answer)
+
