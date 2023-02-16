f9e43b0ff4256f1175b8c571fe4a8e7a401745ae
eunbi <62796977+ceb1127@users.noreply.github.com>
2023-02-16 16:23:54 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,13 +1,11 @@
-import sys
-# 합 100 넘는 2명 빼기
-height = [ int(input()) for i in range(9) ]
-height.sort()
-total = sum(height)
-for i in range(0,len(height)) :
-    for j in range(i+1,len(height)):
-        if total - height[i] - height[j] == 100 : #아홉난쟁이 합- 가짜난쟁이 합 = 100
-            for k in range(len(height)) : 
-                if i == k or j == k: 
-                    continue #가짜 난쟁이들 제외
-                print(height[k])
-            sys.exit(0)
+# combination 라이브러리 활용 9C2
+from itertools import combinations
+
+nanjaeng = [int(input()) for i in range(9)]
+occation = list(combinations(nanjaeng,7))
+for i in occation:
+    if sum(i) == 100 :
+        answer = list(i)
+        break
+answer.sort()
+print(*answer,sep='\n')
