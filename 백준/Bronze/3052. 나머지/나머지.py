a41ae01ad762a3df56630048f43abfc1cd2c2853
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-07-28 15:42:18 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,13 +1,7 @@
-result = []
-count = {}
+numlist = []
 
-for i in range(10) :
-    num = int(input())
-    result.append(num % 42)
-    
-for i in result :
-    try : count[i] += 1 #리스트에 이미 존재하는 값이면 +1
-    except : count[i] = 1 #리스트에 없으면 1 출력 
+for i in range(10):
+    a = int(input())
+    numlist.append(a%42)
 
-
-print(len(count)) # 서로 다른 수 출력 
+print(len(set(numlist)))
