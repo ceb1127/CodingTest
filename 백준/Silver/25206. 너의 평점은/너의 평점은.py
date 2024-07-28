d904267655fcded81e273c4fcf8a0029a4a6d4c6
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-07-28 19:32:11 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,5 +1,5 @@
-level = ['A+','A0','B+','B0','C+','C0','D+','D0','F']
-score = [4.5,4.0,3.5,3.0,2.5,2.0,1.5,1.0,0.0]
+grade = {'A+':4.5, 'A0':4.0, 'B+':3.5, 'B0':3.0, 'C+':2.5, 
+         'C0':2.0, 'D+':1.5, 'D0':1.0, 'F':0.0}
 
 total_score = 0
 result = 0
@@ -8,6 +8,6 @@ for i in range(20):
     sub, sco, gra = input().split()
     if gra != 'P' :
         total_score += float(sco)
-        result += float(sco)*score[level.index(gra)]
+        result += float(sco)*grade[gra]
 
 print('%.6f' %(result/total_score))
