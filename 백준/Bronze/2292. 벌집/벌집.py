249ed3ddca0bef8ec1ac1d6de7472071367e92f8
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-08-04 21:14:11 +0900

    [Bronze II] Title: 벌집, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,13 +1,9 @@
-# # "6의 배수" 로 늘어나는 벌집 -> whlie문으로 입력받은 수인 n에 도달할 때 까지만 반복
-# 반복문이 반복되는 동안에 반복횟수를 카운트해서 이 카운트 한 숫자를 마지막에 출력
+su = int(input())
 
-
-n = int(input())
-nums_pileup = 1 # 벌집 개수 1부터 시작해서 6의 배수 증가
-cnt = 1 # 반복문이 반복되는 횟수 카운트
-
-while n > nums_pileup :
-    nums_pileup += 6 * cnt 
+honey = 1
+cnt = 1
+while su > honey :
+    honey += (cnt*6)
     cnt += 1
-
 print(cnt)
+    
