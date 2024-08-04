f68091e45bf10c9c54cff879b4a1cc0164e47f06
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-08-04 17:26:55 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,10 +1,8 @@
 t = int(input())
-q = 25
-d = 10
-n = 5
-p = 1
 
 for i in range(t):
     c = int(input())
-    print(c//q, c%q//d, c%q%d//n, c%q%d%n//p)
+    for i in [25,10,5,1]:
+        print(c//i, end=' ')
+        c = c%i
     
