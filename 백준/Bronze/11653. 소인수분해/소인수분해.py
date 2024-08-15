701b20cc16e0cc80d387dedee6a33cb0af41bb3e
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-08-15 17:58:55 +0900

    [Bronze I] Title: 소인수분해, Time: 864 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,12 +1,7 @@
 N = int(input())
 
-if N == 1 :
-    print('')
-
-for i in range(2,N+1) : #2부터 나누기
-    if N % i == 0 : # 해당숫자로 나누어 떨어지면
-        while N % i == 0 :
-            print(i)
-            N = N / i
-        
+for i in range(2,N+1):
+    while N % i == 0 :            
+        print(i)
+        N //= i
         
