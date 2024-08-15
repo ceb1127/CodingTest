4195856c11a7aa9e5a4bff8dd26dad28c1ba9a38
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-08-15 18:29:50 +0900

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,15 +1,16 @@
-x_list = []
-y_list = []
+a_list = []
+b_list = [] 
 
-for i in range(3) :
-    x, y = map(int,input().split())
-    x_list.append(x)
-    y_list.append(y)
+for i in range(3):
+    a, b = map(int, input().split())
+    a_list.append(a)
+    b_list.append(b)
 
-for i in range(3) :
-    if x_list.count(x_list[i]) == 1 :
-        x4 = x_list[i]
-    if y_list.count(y_list[i]) == 1 :
-        y4 = y_list[i]
+for i in range(3):
+    if a_list.count(a_list[i]) == 1:
+        a4 = a_list[i]
+    if b_list.count(b_list[i]) == 1:
+        b4 = b_list[i]
+    
 
-print(x4,y4)
+print(a4, b4)
