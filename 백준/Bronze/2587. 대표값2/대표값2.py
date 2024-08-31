5a766540df0a8c5f93546a76f42176fac11a477b
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-09-01 01:39:22 +0900

    [Bronze II] Title: 대표값2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,14 +1,9 @@
 n_list = [] 
-sum_num = 0
 
 for i in range(5) :
-    n = int(input())
-    n_list.append(n)
+    n_list.append(int(input()))
 
 n_list.sort()
 
-for i in n_list:
-    sum_num +=i
-
-print(int(sum_num/len(n_list)))
+print(int(sum(n_list)/len(n_list)))
 print(n_list[2])
