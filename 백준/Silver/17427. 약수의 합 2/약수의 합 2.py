3baa05d73c9be914834bf91aa9cbae731da7748e
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-09-03 20:57:10 +0900

    [Silver II] Title: 약수의 합 2, Time: 176 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
@@ -1,5 +1,9 @@
-n = int(input())
-answer = 0
-for i in range(1,n+1):
-    answer += (n//i)*i
-print(answer)
+# g(2) = f(1) + f(2)  #i의 약수의 합 + 2의 약수의 합
+N = int(input())
+total = 0
+
+for i in range(1,N+1):
+     total += (( N // i )*i) 
+print(total)
+
+#f(1) + f(2) + f(3) ... 
