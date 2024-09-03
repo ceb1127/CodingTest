750b5888aeef879872c9736326daaf066cea164d
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-09-04 01:40:54 +0900

    [Gold IV] Title: 약수의 합, Time: 232 ms, Memory: 126452 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.py
+++ 백준/Gold/17425. 약수의 합/약수의 합.py
@@ -1,29 +1,23 @@
-# 1) D를 모두 구해놓음
-# 2) 입출력
-
 import sys
+input = sys.stdin.readline
+
+T = int(input().strip())
 
 MAX = 1000000 #최대값 설정
-d = [1]*(MAX+1) #DP 1로 초기화
-s = [0]*(MAX+1) #s : 값 누적 리스트
 
-for i in range(2,MAX+1):
-    j = 1
-    while i*j <= MAX:
-        # i의 배수에 값 추가 
-        d[i*j] += i
-        j += 1
+dp = [0]*(MAX+1) #dp 초기화 
+sum_dp = [0]*(MAX+1) #값 누적 리스트 
 
+# 약수의 합 계산
+for i in range(1, MAX+1): # 1 2 3 4 5 
+    for j in range(i,MAX+1,i): # 1 1 1 1 1 2 2 3 4 5 
+        dp[j] += i
+
+#print(dp)
+# 1부터 i까지 약수의 합의 총합 계산
 for i in range(1,MAX+1):
-    s[i] = s[i-1] + d[i] #누적 값 계산
+    sum_dp[i] = sum_dp[i-1]+dp[i]
 
-
-t = int(input())
-answer = []
-
-for _ in range(t) :
-    n = int(sys.stdin.readline())
-    answer.append(s[n])
-print('\n'.join(map(str,answer))+'\n')
-
-# 시간 복잡도 O(nlogn)
+for _ in range(T):
+    n = int(input().strip())
+    print(sum_dp[n])
