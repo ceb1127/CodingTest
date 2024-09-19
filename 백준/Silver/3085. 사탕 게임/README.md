1e5723557978f9818f41da2fa9bd5bc2b6c07767
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-09-20 00:04:08 +0900

    [Silver II] Title: 사탕 게임, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/README.md
+++ 백준/Silver/3085. 사탕 게임/README.md
@@ -1,14 +1,18 @@
-# [Silver III] 사탕 게임 - 3085 
+# [Silver II] 사탕 게임 - 3085 
 
 [문제 링크](https://www.acmicpc.net/problem/3085) 
 
 ### 성능 요약
 
-메모리: 31256 KB, 시간: 120 ms
+메모리: 31120 KB, 시간: 104 ms
 
 ### 분류
 
-브루트포스 알고리즘(bruteforcing), 구현(implementation)
+브루트포스 알고리즘, 구현
+
+### 제출 일자
+
+2024년 9월 20일 00:03:59
 
 ### 문제 설명
 
--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,108 +1,32 @@
-# 임의의 두칸을 변경했을 때 정답에 변화가 있는 것은 최대 3개의 행과 열임을 이용
-
-def check(a, start_row, end_row, start_col, end_col) :
-    n = len(a)
-    answer = 1
-    for i in range(start_row,end_row+1):
-        cnt = 1
-        for j in range(1,n) :
-            if a[i][j] == a[i][j-1] :
-                cnt += 1
-            else :
-                cnt = 1
-            if answer < cnt :
-                answer = cnt
-    for i in range(start_col,end_col+1):
-        cnt = 1
-        for j in range(1,n) :
-            if a[j][i] == a[j-1][i] :
-                cnt += 1
-            else :
-                cnt = 1
-            if answer < cnt :
-                answer = cnt
-    return answer
-
-
-n = int(input())
-a = [list(input()) for _ in range(n)]
-answer = 0
-for i in range(n):
-    for j in range(n):
-        if j+1 < n :
-            a[i][j], a[i][j+1] = a[i][j+1], a[i][j]
-            temp = check(a, i, i, j, j+1 )
-            if answer < temp:
-                answer = temp
-            a[i][j], a[i][j+1] = a[i][j+1],a[i][j]
-        if i+1 < n:
-            a[i][j], a[i+1][j] = a[i+1][j],a[i][j]
-            temp = check(a, i, i+1, j, j)
-            if answer < temp:
-                answer = temp
-            a[i][j], a[i+1][j] = a[i+1][j],a[i][j]
-print(answer)
-
-''' 
- 1 ) 왜 시간초과 ? 
- # 시간복잡도 먼저 계산해보고 부르트포수(완전탐색)인거 확인 . 1억 = 1초
 import sys
 input = sys.stdin.readline
-# 몇개 먹을 수 있는지 찾는 함수
-def check(a) :
-    n = len(a)
-    answer = 1
 
-    #한칸(i,j) 골라서 인접한 오른쪽, 아래 검사
-    for i in range(n) : 
-        #가로줄 순회
-        cnt = 1 # 첨 시작
-        for j in range(1,n):
-            cnt = 1 
-            for j in range(1,n) :
-                if a[i][j] == a[i][j-1] : #이전과 같다면 +1
-                    cnt += 1
-                else :
-                    cnt = 1   # 다르면 1
-                # 현재 cnt가 더 크면 answer 갱신
-                if answer < cnt :
-                    answer = cnt
-
-            #세로줄 순회
+def count(lst):
+    cnt = ans = 1
+    for i in range(1,len(lst)):
+        if lst[i] == lst[i-1]:
+            cnt+=1 
+            ans = max(ans, cnt)
+        else:
             cnt = 1
-            for j in range(1,n):
-                if a[j][i]== a[j-1][i] : 
-                    cnt += 1
-                else: 
-                    cnt = 1
-                if answer < cnt :
-                    answer = cnt
-    return answer
+    return ans
 
-n = int(input())
-a = [list(input()) for _ in range(n)]
-answer = 0
+def solve(arr):
+    mx = 0
+    for i in range(N-1):
+        for j in range(0,N):
+            arr[i][j],arr[i][j+1] = arr[i][j+1],arr[i][j]
+            mx = max(mx,count(arr[i]))
+            arr[i][j],arr[i][j+1] = arr[i][j+1],arr[i][j]
 
-for i in range(n):
-    for j in range(n):
-    # 행 : 오른쪽 교환 -> 계산 . 최대값이면 적용 -> 원래상태로 돌아가기 
-        if j+1 < n :
-            a[i][j],a[i][j+1] = a[i][j+1],a[i][j] #바꾸기 swap
-            temp = check(a) #인접한 것을 바꿨을 때 가장 긴 연속 부분 찾아내는 함수 check
-            if answer < temp:
-                answer = temp
-            a[i][j],a[i][j+1] = a[i][j+1],a[i][j] #원래대로
-    # 열 : 아래 교환 -> 계산 . 최대값이면 계산 -> 원래상태로 돌아가기
-        if i+1 < n :
-            a[i][j],a[i+1][j] = a[i+1][j],a[i][j]
-            temp = check(a)
-            if answer < temp:
-                answer = temp
-            a[i][j],a[i+1][j] = a[i+1][j],a[i][j]
-print(answer)
+            arr[i][j],arr[i+1][j] = arr[i+1][j],arr[i][j]
+            mx = max(mx,count(arr[i]), count(arr[i+1]))
+            arr[i][j],arr[i+1][j] = arr[i+1][j],arr[i][j]
 
+    return mx
 
-'''
-
-
- 
+N = int(input())
+arr = [list(input())+[0] for _ in range(N)]+[[0]*(N+1)]
+arr_t = list(map(list, zip(*arr)))
+ans = max(solve(arr),solve(arr_t))
+print(ans)
