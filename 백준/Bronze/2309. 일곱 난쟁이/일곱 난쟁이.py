ce4766a7cd935ae25c0e556f1d32f4589a886721
eunbi <62796977+ceb1127@users.noreply.github.com>
2024-09-22 17:54:05 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,15 +1,18 @@
 import sys
 input = sys.stdin.readline
-nan = []
 
-for _ in range(9):
-    nan.append(int(input().strip()))
+lst = [int(input()) for _ in range(9)]
 
-from itertools import combinations
-nan_final = list(combinations(nan,7))
+def solve(): 
+    N = 9
+    num = sum(lst) - 100
+    for i in range(0, N-1):
+        for j in range(i+1, N):
+             if lst[i] + lst[j] == num :
+                 return lst[i], lst[j]
 
-for i in nan_final:
-    if sum(i) == 100:
-        for j in sorted(i):
-            print(j)
-        break
+n, m = solve()
+for i in sorted(lst):
+    if i != n and i != m :
+        print(i)
+
